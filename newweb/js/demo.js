aa77f376286c45acd5e4a03f78c23ae61ddfecdc
ORBenjamin <or123123@gmail.com>
2017-08-12 14:56:29 +0300

    set, save, send function
    turn off button is now function as intended

--- newweb/js/demo.js
+++ newweb/js/demo.js
@@ -2,49 +2,73 @@ fixScale(document);
 
 var val = document.getElementById('rgbValue');
 var socket = io.connect();
-
+var hexi;
+var hardset;
+var fadeoff;
 var joe = colorjoe.rgb('rgbPicker', nowcolor)
 socket.on('hex', function(hexs) {
-    var newhex = hexs.hex;
-    console.log('connact! ' + newhex)
-    joe.set(newhex);
+  var newhex = hexs.hex;
+//  console.log('connact! ' + newhex)
+  joe.set(newhex);
 });
 
 joe.on('change', function(c) {
-    val.innerHTML = c.css();
-    var cred = Math.round(255 * c.red());
-    var cgreen = Math.round(255 * c.green());
-    var cblue = Math.round(255 * c.blue());
+  hexi = c.hex();
+  val.innerHTML = c.css();
+  var cred = Math.round(255 * c.red());
+  var cgreen = Math.round(255 * c.green());
+  var cblue = Math.round(255 * c.blue());
 
-    console.log('R:' + cred + ' G:' + cgreen + ' B:' + cblue);
+//  console.log('R:' + cred + ' G:' + cgreen + ' B:' + cblue);
 
 
 
-    //socket io
-    socket.emit('color', {
-        red: cred,
-        green: cgreen,
-        blue: cblue
-    });
+  //socket io
+  socket.emit('color', {
+    red: cred,
+    green: cgreen,
+    blue: cblue
+  });
 
 
 
 }).update();
 
 joe.on('done', function(c) {
-    console.log('I am Done.');
-    var chex = c.hex();
+//  console.log('I am Done.');
+  var chex = c.hex();
+  socket.emit('hex', {
+    hex: chex
+  });
+
+  socket.on('hex', function(hexs) {
+    var newhex = hexs.hex;
+//    console.log('new! ' + newhex)
+    joe.set(newhex);
+  });
+
+
+
+
+});
+
+hardset = function(r, g, b) {
+    console.log('rgb(' + r + ',' + g + ',' + b + ')');
+    joe.set('rgb(' + r + ',' + g + ',' + b + ')');
     socket.emit('hex', {
-        hex: chex
+      hex: hexi
     });
+  };
 
-    socket.on('hex', function(hexs) {
-        var newhex = hexs.hex;
-        console.log('new! ' + newhex)
-        joe.set(newhex);
-    });
+fadeoff = function (r, g, b){
+  do {
+  rz =  Math.max(0, r--);
+  gz =  Math.max(0, g--);
+  bz =  Math.max(0, b--);
+  retur('rgb(' + rz + ',' + gz + ',' + bz + ')');
+  console.log(rz +' '+ gz +' ' + bz);
+} while (rz,gz,bz > 0);
 
+};
 
-
-
-})
+console.log('hi');
