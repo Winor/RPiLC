df09cad0d4e2544784cad48582b67a2cd36a984a
ORBenjamin <or123123@gmail.com>
2018-02-15 23:39:25 +0200

    added a option to disable debug logs

--- winowatch.js
+++ winowatch.js
@@ -1,3 +1,7 @@
+const fs = require("fs");
+const configfile = fs.readFileSync("config.json");
+const config = JSON.parse(configfile);
+
 function getdate() {
   let date = new Date()
   let options = {
@@ -8,34 +12,23 @@ function getdate() {
     hour: "2-digit",
     minute: "2-digit"
   };
-  dforamt = date.toLocaleTimeString("en-us", options);
+  return date.toLocaleTimeString("en-us", options);
 }
-
-getdate();
 
 module.exports.info = function(msg) {
-  getdate();
-  console.log(dforamt + "\x1b[36m", "[info]", "\x1b[0m" + "" + msg);
-
+  console.log(getdate() + "\x1b[36m", "[info]", "\x1b[0m" + "" + msg);
 }
-
 
 module.exports.debug = function(msg) {
-  getdate();
-  console.log(dforamt + "\x1b[35m", "[debug]", "\x1b[0m" + "" + msg);
-
+  if (config.debug == true) {
+    console.log(getdate() + "\x1b[35m", "[debug]", "\x1b[0m" + "" + msg);
+  }
 }
-
 
 module.exports.warn = function(msg) {
-  getdate();
-  console.log(dforamt + "\x1b[33m", "[warn]", "\x1b[0m" + "" + msg);
-
+  console.log(getdate() + "\x1b[33m", "[warn]", "\x1b[0m" + "" + msg);
 }
 
-
 module.exports.error = function(msg) {
-  getdate();
-  console.log(dforamt + "\x1b[31m", "[error]", "\x1b[0m" + "" + msg);
-
+  console.log(getdate() + "\x1b[31m", "[error]", "\x1b[0m" + "" + msg);
 }
