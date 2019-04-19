54b8e866ebabd506d73a317247a785238a799eaf
ORBenjamin <or123123@gmail.com>
2019-04-19 16:26:32 +0300

    acts as a database for RPiLC

---
+++ src/data.js
@@ -0,0 +1,26 @@
+'use strict'
+module.exports = {
+  Version: "1.0.0",
+  type: 'desktop',
+  description: "My Workstation",
+  state: "off",
+  ColorVals: {
+    CurrentHEX: '#000000',
+    CurrentRGB: {
+      r: 0,
+      g: 0,
+      b: 0
+    },
+    CurrentCName: 'black'
+  },
+  CycleMode: {
+    state: false, // true / false
+    colors: ['#be0000', '#beb500', '#21be00', '#0051be'],
+    speed: 8000,
+    effect: "fade"
+  },
+  RainbowMode: {
+    state: false, // true / false
+    speed: 6000
+  }
+}
