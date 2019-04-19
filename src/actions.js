3916d65cd5335a1110b276f874a3b3c411831580
ORBenjamin <or123123@gmail.com>
2019-04-19 16:24:42 +0300

    exports action functions

---
+++ src/actions.js
@@ -0,0 +1,36 @@
+'use strict'
+let data = require("./data.js");
+const sioserver = require('./sioserver.js');
+const logger = require('./logger.js');
+const gpio = require('./gpio.js');
+const logic = require('./logic.js');
+const effect = require('./effects.js');
+
+module.exports = {
+  connection: function() {
+    logger.debug("New client Connected.");
+    sioserver('data', data);
+    logger.debug("Sent data to new client.");
+  },
+  change: function(c) {
+    gpio(c.r, c.g, c.b);
+  },
+  done: function(val) {
+    logic.setcolor(val);
+  },
+  cycle: function(cycle) {
+    logger.debug("Got a list of colors to "+ cycle.effect + ": " + cycle.colors + ". Speed: " + cycle.speed + ". the state is now " + cycle.state);
+    data.CycleMode = cycle;
+    sioserver('CycleSync', data.CycleMode);
+    effect.CycleModeSwitch(cycle.effect);
+  },
+  togglestate: function() {
+    if (data.CycleMode.state) {
+      data.CycleMode.state = false;
+      logger.debug("CycleMode state is now " + data.CycleMode.state);
+      sioserver('CycleSync', data.CycleMode);
+    }
+    logic.chgstate();
+  }
+
+}
