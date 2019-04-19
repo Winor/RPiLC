0442f4d3b33832b2fc2308f00c176827c7b0bec5
ORBenjamin <or123123@gmail.com>
2019-04-19 16:28:17 +0300

    functions to interact with the pi's general purpose input output

---
+++ src/gpio.js
@@ -0,0 +1,29 @@
+'use strict'
+const logger = require('./logger.js');
+// load config file
+const fs = require("fs");
+const configfile = fs.readFileSync("config.json");
+const config = JSON.parse(configfile);
+logger.debug('Config File Loaded.');
+// require pigpio and set PWM gpio numbers
+const Gpio = require('pigpio').Gpio,
+  red = new Gpio(config.redgpio, {
+    mode: Gpio.OUTPUT
+  }),
+  green = new Gpio(config.greengpio, {
+    mode: Gpio.OUTPUT
+  }),
+  blue = new Gpio(config.bluegpio, {
+    mode: Gpio.OUTPUT
+  });
+// set value to zero
+  red.digitalWrite(0);
+  green.digitalWrite(0);
+  blue.digitalWrite(0);
+
+// function to write PWM value
+module.exports = function (r, g, b) {
+  red.pwmWrite(r);
+  green.pwmWrite(g);
+  blue.pwmWrite(b);
+};
