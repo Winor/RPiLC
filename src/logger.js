196fa271903984b20f2c0118e1681065519bff65
ORBenjamin <or123123@gmail.com>
2019-04-19 16:16:34 +0300

    added an option to log raw data

---
+++ src/logger.js
@@ -0,0 +1,40 @@
+'use strict'
+const fs = require("fs");
+const configfile = fs.readFileSync("config.json");
+const config = JSON.parse(configfile);
+
+function getdate() {
+  let date = new Date()
+  let options = {
+    month: "short",
+    day: "numeric"
+
+  };
+  return date.toLocaleTimeString("en-us", options);
+}
+
+
+
+module.exports.info = function(msg) {
+  console.log(getdate() + "\x1b[36m", "[info]", "\x1b[0m" + "" + msg);
+}
+
+module.exports.debug = function(msg) {
+  if (config.debug) {
+    console.log(getdate() + "\x1b[35m", "[debug]", "\x1b[0m" + "" + msg);
+  }
+}
+
+module.exports.data = function(msg) {
+  if (config.debug) {
+    console.log(getdate() + "\x1b[35m", "[debug]", "\x1b[0m" + "" + JSON.stringify(msg, null, 2));
+  }
+}
+
+module.exports.warn = function(msg) {
+  console.log(getdate() + "\x1b[33m", "[warn]", "\x1b[0m" + "" + msg);
+}
+
+module.exports.error = function(msg) {
+  console.log(getdate() + "\x1b[31m", "[error]", "\x1b[0m" + "" + msg);
+}
