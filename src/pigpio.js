79f63e559c4ee14d415c8510f35a8ccd3e8f4138
Winor <or123123@gmail.com>
2020-02-09 14:58:51 +0000

    Light class now acts as Logic for RGB led strip

--- src/pigpio.js
+++ src/pigpio.js
@@ -1,20 +1,19 @@
+/* eslint-disable no-new-func */
 'use strict'
-const logger = require('./logger.js');
-logger.debug('Config File Loaded.');
-const gpio = require('pigpio').Gpio;
+const logger = require('./logger.js')
+logger.debug('Config File Loaded.')
+const Gpio = require('pigpio').Gpio
 
-module.exports = function(int) {
-      let led =  String(Object.keys(int));
-      let pin = {}
-      let cmd = ""
-      Object.keys(int[led]).forEach(function (item) {
-        pin[item] = new gpio(int[led][item], {mode: gpio.OUTPUT})
-        pin[item].digitalWrite(0)
-        cmd += "this.io."+item+".pwmWrite("+item+");"
-      }); 
-      this.type = led;
-      this.io = pin
-      this.set = new Function (String(Object.keys(int[led])) ,cmd)
-  }
-
-
+module.exports = function (int) {
+  const led = String(Object.keys(int))
+  const pin = {}
+  let cmd = ''
+  Object.keys(int[led]).forEach(function (item) {
+    pin[item] = new Gpio(int[led][item], { mode: Gpio.OUTPUT })
+    pin[item].digitalWrite(0)
+    cmd += 'this.io.' + item + '.pwmWrite(' + item + ');'
+  })
+  this.type = led
+  this.io = pin
+  this.set = new Function(String(Object.keys(int[led])), cmd)
+}
